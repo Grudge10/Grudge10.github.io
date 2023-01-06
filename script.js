3bbd8fdb09d5ecd1104badd48d728fcac23569d6
Grudge10 <100700937+Grudge10@users.noreply.github.com>
2023-01-06 21:38:04 +0800

    fade on cover page

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ window.addEventListener("load", () => {
   });
 });
 
+let image = document.querySelector('.hero');
+
+window.addEventListener('scroll', function () {
+  let value = 1 + window.scrollY/-600;
+  image.style.opacity = value; 
+})
+
 var typed = new Typed(".typing-hero", {
   strings: ["Jan Liam.", "A Web Developer.", "Creative.", "Cool."],
   typeSpeed: 70,
